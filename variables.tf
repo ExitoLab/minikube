17e50ade857a41ddde75fe35713eda2ef5ca11ed
Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>
2021-05-12 16:32:39 +0400

    feat: improve minikube creating, create vpc and subnet on the fly
    Signed-off-by: Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>

--- variables.tf
+++ variables.tf
@@ -4,3 +4,14 @@ variable "aws_region" {
   default     = "us-east-1"
   description = "The region of the aws account"
 }
+
+variable "vpc_cidr" {
+  type    = string
+  default = "172.18.0.0/16"
+}
+
+variable "subnet_cidr_1" {
+  type        = string
+  description = "Subnet cidr"
+  default     = "172.18.0.0/24"
+}
